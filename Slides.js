a28b71074bf7459409f7d5387bbaf45305738893
Stephenharry19 <stephendee90@gmail.com>
2025-09-22 04:02:10 +0100

    Create Slides.js
    You are tasked with creating a simple React application called "Basic Sideshow that displays a series of slides and allows users to navigate through them. The application requirements are detailed below, and the finished application must pass all of the unit tests.

---
+++ Slides.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function Slides({ slides }) {
+  // Tracking the current slide index
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // The Handlers for buttons
+  const handleRestart = () => {
+    setCurrentIndex(0);
+  };
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentIndex < slides.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          onClick={handleRestart}
+          disabled={currentIndex === 0}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={handlePrev}
+          disabled={currentIndex === 0}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={handleNext}
+          disabled={currentIndex === slides.length - 1}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{slides[currentIndex].title}</h1>
+        <p data-testid="text">{slides[currentIndex].text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Slides;
